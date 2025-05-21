f40715b08432b6ca6e32113b8b9e23e9b996409c
Willian Wallace <willwallacehora@gmail.com>
2025-05-21 16:30:11 -0300

    Adicionado biblioteca do Lucide React e reestruturação do arquivo app transformando os Headers e containers em componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import './styles/theme.css';
+import './styles/global.css';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
+
 export function App() {
   return (
     <>
-      <h1>Olá mundo (do APP)</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-        porro cumque nesciunt sapiente aut architecto tempora tempore saepe?
-        Repellendus mollitia ad consectetur saepe nam. Quam unde quo odio.
-        Ipsum, ea.
-      </p>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
